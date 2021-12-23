55b80c0cf43ff368775eb5579e415808f53b6b85
dalbodeule <jioo0224@naver.com>
2021-12-23 14:28:35 +0900

    add screen/python3 on cuda image

--- ubuntu-cuda-11-with-node-latest.sh
+++ ubuntu-cuda-11-with-node-latest.sh
@@ -3,4 +3,4 @@ docker_username="dalbodeule"
 image_name="ubuntu-cuda-11-with-node-latest"
 version=$(date '+%Y-%m-%d')
 
-docker build -f ./${image_name}.dockerfile --tag ${docker_username}/${image_name}:${version} ./
+docker build --no-cache -f ./${image_name}.dockerfile --tag ${docker_username}/${image_name}:${version} ./
