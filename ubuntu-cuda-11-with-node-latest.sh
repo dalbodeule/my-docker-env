7d53b59b4a920282c8a53c367d28b39fe4ca1a40
dalbodeule <jioo0224@naver.com>
2021-12-23 13:33:44 +0900

    fix sh file

--- ubuntu-cuda-11-with-node-latest.sh
+++ ubuntu-cuda-11-with-node-latest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker_username = "dalbodeule"
-image_name = "ubuntu-cuda-11-with-node-latest"
-version = $(date '+%Y-%m-%d %H:%M:%S')
+docker_username="dalbodeule"
+image_name="ubuntu-cuda-11-with-node-latest"
+version=$(date '+%Y-%m-%d')
 
-docker build --tag ${docker_username}/${image_name}:${version}
+docker build -f ./${image_name}.dockerfile --tag ${docker_username}/${image_name}:${version} ./
