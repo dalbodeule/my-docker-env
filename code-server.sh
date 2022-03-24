edadb47ebf9ef1df16b80bd7b78ae4f68b84f491
dalbodeule <jioo0224@naver.com>
2022-03-24 19:39:17 +0900

    add latest tag + openjdk-17

--- code-server.sh
+++ code-server.sh
@@ -4,4 +4,4 @@ image_name="code-server"
 version=$(date '+%Y-%m-%d')
 docker_gid=$(getent group docker | cut -d: -f3)
 
-docker build --no-cache -f ./${image_name}.dockerfile --tag ${docker_username}/${image_name}:${version} --build-arg DOCKER_GID=${docker_gid} ./
+docker build --no-cache -f ./${image_name}.dockerfile --tag ${docker_username}/${image_name}:${version} --tag ${docker_username}/${image_name}:latest --build-arg DOCKER_GID=${docker_gid} ./
